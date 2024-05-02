f9b3c05311a15d7f0d58f730a695bd5a59bd7ff6
noahwekesa <noahwekesa0@gmail.com>
2024-05-02 14:15:36 +0300

    fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,12 +3,14 @@
 modeule for Type Annotation
 """
 
-from typing import List
+import typing
 
 
-def element_length(lst: List[str]) -> List[int]:
+def element_length(
+    lst: typing.Iterable[typing.Sequence],
+) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """
     Function that takes a list of strings as argument
     And returns a list of integers representing the lengths of the strings
     """
-    return [len(i) for i in lst]
+    return [(i, len(i)) for i in lst]
