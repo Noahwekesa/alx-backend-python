ed465cfae3e23197dc7a2a885cbc6aff464b4d62
noahwekesa <noahwekesa0@gmail.com>
2024-05-02 14:09:12 +0300

    pycode

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-module that takes a string k and an int OR float v as arguments and returns a tuple
+Module that takes a string k and an int OR float v as arguments
+and returns a tuple
 """
 
 from typing import Union, Tuple
@@ -8,6 +9,7 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Function that takes a string k and an int OR float v as arguments and returns a tuple
+    Function that takes a string k and an int OR float v as arguments
+    And returns a tuple
     """
     return (k, v**2)
