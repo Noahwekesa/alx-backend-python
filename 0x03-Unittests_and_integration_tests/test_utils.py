b50cc91b5afe165637c9e3bd2e71d482aa87f1ba
noahwekesa <noahwekesa0@gmail.com>
2024-05-31 12:47:13 +0300

    para

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,19 +4,29 @@ This is a test file for the utils module
 """
 
 import unittest
+from utils import access_nested_map
 from parameterized import parameterized
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TaskAccessNestedMap(unittest.TestCase):
+    """
+    This class tests the access_nested_map function
+    """
+
     @parameterized.expand(
         [
-            ({"a": 1}, ("a",)),
-            ({"a": {"b": 2}}, ("a",)),
-            ({"a": {"b": 2}}, ("a", "b")),
+            ({"a": 1, "b": {"c": 2}}, ["a"], 1),
+            ({"a": 1, "b": {"c": 2}}, ["b", "c"], 2),
+            ({"a": 1, "b": {"c": 2}}, ["a", "b"], None),
+            ({"a": 1, "b": {"c": 2}}, ["c", "d"], None),
         ]
     )
-    def test_access_map(self, nested_map, path):
-        expected_value = nested_map.get(path[0])
-        for key in path[1:]:
-            expected_value = expected_value.get(key)
-        self.assertEqual(expected_value, access_nested_map(nested_map, path))
+    def test_access_nested_map(self):
+        """
+        This method tests the access_nested_map function
+        """
+        data = {"a": 1, "b": {"c": 2}}
+        self.assertEqual(access_nested_map(data, ["a"]), 1)
+        self.assertEqual(access_nested_map(data, ["b", "c"]), 2)
+        self.assertIsNone(access_nested_map(data, ["a", "b"]))
+        self.assertIsNone(access_nested_map(data, ["c", "d"]))
